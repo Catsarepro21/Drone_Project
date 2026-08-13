62a761cbab28ee48698dba8aaa896a1bb6b789a9
Rehan Abbu <rehanhabbu@gmail.com>
2026-08-13 14:07:53 +0000

    Fix Test_vision.py

--- code/src/test_vision.py
+++ code/src/test_vision.py
@@ -6,7 +6,7 @@ def run_test():
     tracker = VisionTracker(is_sim=True)
     dummy_frame = 225 * np.ones((480, 640, 3), dtype=np.uint8)
 
-    cv2.circle(dummy_frame, (320, 240), 5, (0, 0, 0) -1)
+    cv2.circle(dummy_frame, (320, 240), 5, (0, 0, 0), -1)
 
     print("[Test] Running vision tracker test with dummy frame...")
     frame, angle_x, angle_y, found = tracker.process_frame(dummy_frame)
